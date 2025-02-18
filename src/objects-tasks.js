b0a79f6347ee33de02f4d5c4115e5db65bc88e77
Lorenzo-StJohn <britishaccent@bk.ru>
2025-02-18 14:05:07 +0400

    feat: implement removeProperties function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -72,8 +72,12 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const ans = obj;
+  keys.forEach((key) => {
+    delete ans[key];
+  });
+  return ans;
 }
 
 /**
